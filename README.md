63426b03baba5711b5175fbe5c9feb77268958bb
Sjors Snoeren <mail@sjorssnoeren.com>
2017-08-30 17:04:58 +0200

    Adds initial experiment

---
+++ README.md
@@ -0,0 +1,20 @@
+FontFeest
+=========
+
+Painless cross-browser font-face in 1 line JavaScript. **This is a work in progress**
+
+As of today, implementing font-faces is still a cumbersome task to do. Most of us go to an online conversion site to make it happen. With the current state of JavaScript, we must be able to do it quicker and simpler. Welcome FontFeest.
+
+This is all it takes to generate all your required formats. Just hand in a .ttf file and the other files as well as the matching CSS is generated automatically.
+
+```
+fontfeest('./fixtures/OpenSans-Light.ttf', 'Open Sans', 300).then((css) => {
+  console.log(css);
+});
+```
+
+## Roadmap
+
+* Try integrating with the Webpack enviroment to make it easy to output CSS at the right place.
+* Explore ways to integrate with node require and eventually a Webpack Plugin.
+* Try if it's possible to fetch data from the fonts itself. Font weights, families and styles may or may not be enclosed in the font files already.
---
+++ index.js
@@ -0,0 +1,55 @@
+const fs = require('fs');
+const path = require('path');
+
+const Promise = require('bluebird');
+
+const transformers = {
+  eot: require('ttf2eot'),
+  woff: require('ttf2woff'),
+  woff2: require('ttf2woff2'),
+};
+
+const transformFontTo = (ext, ttfBuffer) => {
+  const func = transformers[ext];
+  const result = func(new Uint8Array(ttfBuffer));
+  return new Buffer(result.buffer);
+};
+
+const writeTransformedFonts = (dirname, filename, ttfBuffer) => {
+  const writeFile = Promise.promisify(fs.writeFile);
+
+  const promises = Object.keys(transformers).map((ext) => {
+    const buffer = transformFontTo(ext, ttfBuffer);
+    const filepath = `./${dirname}/${filename}.${ext}`;
+    return writeFile(filepath, buffer);
+  });
+
+  return Promise.all(promises);
+};
+
+const createCSSFontFaceTemplate = (dirname, filename, styles) => {
+  return `
+    @font-face {
+      font-family: ${styles.fontFamily};
+      font-weight: ${styles.fontWeight};
+      font-style: ${styles.fontStyle};
+      src: url('${dirname}/${filename}.eot');
+      src: url('${dirname}/${filename}.woff2') format('woff2'),
+          url('${dirname}/${filename}.woff') format('woff'),
+          url('${dirname}/${filename}.ttf')  format('truetype');
+    }
+  `;
+};
+
+const fontfeest = function fontfeest(file, fontFamily, fontWeight = 400, fontStyle = 'normal') {
+  const dirname = path.dirname(file);
+  const filename = path.basename(file, '.ttf');
+  const ttfBuffer = fs.readFileSync(file);
+
+  return writeTransformedFonts(dirname, filename, ttfBuffer).then(() => {
+    const style = { fontFamily, fontWeight, fontStyle };
+    return createCSSFontFaceTemplate(dirname, filename, style);
+  });
+};
+
+module.exports = fontfeest;
